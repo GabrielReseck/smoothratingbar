530b7973817766e7dfa89bc362b566010a3abd79
GabrielReseck <gsareseck@gmail.com>
2021-07-03 02:59:18 -0300

    Updated sdk version

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -2,6 +2,7 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=G:\Programas\flutter"
 export "FLUTTER_APPLICATION_PATH=G:\Projects\SiteThing\forks\SmoothStarRating\smoothratingbar\example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
